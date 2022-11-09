72516ce8f86d98b311b1028543feeb3248c11931
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-11-09 15:59:02 +0100

    Link + import Link

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import '../styles/Error404.css';
+import { Link } from 'react-router-dom'
 
 
 const Error404 = () => (
     <div className="Error404">
         <h1 className="Error404_code">404</h1>
         <p className="Error404_message">Oups! La page que vous demandez n'existe pas.</p>
-        <link to="/" className="Error404_link">Retourner sur la page d'accueil</link>
+        <Link to="/" className="Error404_link">Retourner sur la page d'accueil</Link>
     </div>
 )
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import logo from '../assets/logo.png'
 import '../styles/Header.css'
+import { Link } from 'react-router-dom'
 
 export function Header() {
     return (
         <div className='banner'>
             <img src={logo} alt='logo KASA' className='lmj-logo' />
             <ul  className='lienMenu'>
-                <li className='lmj-title'><link to="/">Accueil</link></li>
-                <li className='lmj-title'><link to="/a-propos">A propos</link></li>
+                <li className='lmj-title'><Link to="/">Accueil</Link></li>
+                <li className='lmj-title'><Link to="/a-propos">A propos</Link></li>
             </ul>
         </div>
     )
